d156f27b991f338fb7b990e76049ead2fa99d6fa
cwchoi <cwchoi@osci.kr>
2025-02-10 10:19:47 +0900

    주석 추가

--- service/article-read/src/main/java/cwchoiit/board/articleread/config/CacheConfig.java
+++ service/article-read/src/main/java/cwchoiit/board/articleread/config/CacheConfig.java
@@ -15,10 +15,12 @@ import java.util.Map;
 public class CacheConfig {
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        // Redis 에서 캐시로 저장할 cacheName 을 지정, 나는 게시글 조회수를 캐싱할 것이기 때문에 "articleViewCount" 를 지정했고, 원한다면 더 추가할 수 있음 Map 안에
+        Map<String, RedisCacheConfiguration> articleViewCountCache =
+                Map.of("articleViewCount", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(1)));
+
         return RedisCacheManager.builder(connectionFactory)
-                .withInitialCacheConfigurations(
-                        Map.of("articleViewCount", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(1)))
-                )
+                .withInitialCacheConfigurations(articleViewCountCache)
                 .build();
     }
 }
--- service/article-read/src/main/java/cwchoiit/board/articleread/repository/ArticleQueryModel.java
+++ service/article-read/src/main/java/cwchoiit/board/articleread/repository/ArticleQueryModel.java
@@ -18,6 +18,11 @@ public class ArticleQueryModel {
     private Long articleCommentCount;
     private Long articleLikeCount;
 
+    /**
+     * 게시글이 최초 생성된 경우에 대한 이벤트를 받을 때 사용될 팩토리 메서드.
+     * @param payload 게시글 생성 데이터
+     * @return {@link ArticleQueryModel}
+     */
     public static ArticleQueryModel create(ArticleCreatedEventPayload payload) {
         ArticleQueryModel articleQueryModel = mappingArticleQueryModel(
                 payload.getArticleId(),
@@ -33,6 +38,14 @@ public class ArticleQueryModel {
         return articleQueryModel;
     }
 
+    /**
+     * 기존에 있는 게시글을 현재 게시글 조회 최적화 서비스에서 찾지 못한 경우, 실제 서비스에서 데이터를 가져온다.
+     * 그 가져온 데이터를 게시글 조회 최적화 서비스의 데이터베이스인 {@code Redis}에 저장할 때 사용되는 팩토리 메서드.
+     * @param article 게시글 데이터
+     * @param articleCommentCount 게시글 댓글 수
+     * @param articleLikeCount 게시글 좋아요 수
+     * @return {@link ArticleQueryModel}
+     */
     public static ArticleQueryModel create(ArticleClient.ArticleResponse article,
                                            Long articleCommentCount,
                                            Long articleLikeCount) {
